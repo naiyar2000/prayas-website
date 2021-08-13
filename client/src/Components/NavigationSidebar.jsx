f9e58976314618be57d4fc1a79c9058433c78647
naiyar2000 <naiyarimam.2000@gmail.com>
2021-08-14 02:11:15 +0530

    Naiyar. routes and components created

--- client/src/Components/NavigationSidebar.jsx
+++ client/src/Components/NavigationSidebar.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import logo from '../Assets/logo.png'
 
 const NavigationSidebar = () => {
     return (
-        <div style={{height: '100vh',display: 'flex', flexDirection: 'column', backgroundColor: 'blue'}}>
+        <div style={{height: '100vh',display: 'flex', flexDirection: 'column'}}>
+            <div className="child"><img src={logo} alt="" height="60" width="60"/></div>
             <div><Link to='/'>Service</Link></div>
             <div><Link to='/payment'>Payment</Link></div>
             <div><Link to='/booking'>Booking</Link></div>
