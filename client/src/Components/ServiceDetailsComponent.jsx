42fb997cb028335e5036d0a92e5b2e30ac0ffe12
naiyar2000 <naiyarimam.2000@gmail.com>
2021-08-19 23:52:38 +0530

    Naiyar ui changes

--- client/src/Components/ServiceDetailsComponent.jsx
+++ client/src/Components/ServiceDetailsComponent.jsx
@@ -1,24 +1,31 @@
 import React from 'react'
 import serviceImage from '../Assets/serviceImage.jpg'
+import './css/ServiceDetailsComponent.css'
 
 const ServiceDetailsComponent = () => {
     return (
         <div className="card">
             <div className="heading">Service Details</div>
-            <div className="photoColumn" style={{display: 'flex'}}>
-                <div className="image">
+            <div className="photoColumn">
+                <div className="imageFile">
                     <img src={serviceImage} alt="" />
                 </div>
-                <div>
-                    <div className="serviceCode">
-                        <p>Service code : <span className="code">#2389046</span></p>
+                <div className="photoRightSide">
+                    <div>
+                        <div className="serviceCode1">
+                            <p>Service code : <span className="code">#2389046</span></p>
+                        </div>
+                        <h1 style={{margin: '0px', padding: '0px'}}>Perfect salon</h1>
+                        <div className="serviceCategory1">
+                            <p>Service category : <span className="category">Salon</span></p>
+                        </div>
+                        <div className="address1">
+                            <p>shop no. 125, sec 20 near mandir</p>
+                        </div>
                     </div>
-                    <h1>Perfect salon</h1>
-                    <div className="serviceCategory">
-                        <p>Service category : <span className="category">Salon</span></p>
-                    </div>
-                    <div className="address">
-                        <p>shop no. 125, sec 20 near mandir</p>
+                    <div>
+                        <p>+91 608080444</p>
+                        <p>abc_@gmail.com</p>
                     </div>
                 </div>
             </div>
